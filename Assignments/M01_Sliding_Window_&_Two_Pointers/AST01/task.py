6df0c5ff7d2622d48c7869506c21494df53ed276
Samyuktha <samyukthachelli12@gmail.com>
2026-08-25 20:38:52 +0530

    Ass

--- Assignments/M01_Sliding_Window_&_Two_Pointers/AST01/task.py
+++ Assignments/M01_Sliding_Window_&_Two_Pointers/AST01/task.py
@@ -1,3 +1,4 @@
+
 def Check_Palindrome(n: int, s: str) -> bool:
     def is_palindrome(left, right):
         while left < right:
@@ -6,21 +7,14 @@ def Check_Palindrome(n: int, s: str) -> bool:
             left += 1
             right -= 1
         return True
-
-    left = 0
-    right = n - 1
-
+    left, right = 0, n - 1
     while left < right:
         if s[left] != s[right]:
             return is_palindrome(left + 1, right) or is_palindrome(left, right - 1)
-
         left += 1
         right -= 1
-
     return True
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     n = int(input())
-    s = input()
+    s = input().strip()
     print(Check_Palindrome(n, s))
--- Assignments/M01_Sliding_Window_&_Two_Pointers/AST02/task.py
+++ Assignments/M01_Sliding_Window_&_Two_Pointers/AST02/task.py
@@ -1,13 +1,10 @@
 def countGoodSubstrings(s: str) -> int:
     count = 0
-
     for i in range(len(s) - 2):
-        if s[i] != s[i + 1] and s[i] != s[i + 2] and s[i + 1] != s[i + 2]:
+        substring = s[i:i + 3]
+        if len(set(substring)) == 3:
             count += 1
-
     return count
-
-
-if __name__ == '__main__':
-    s = input()
+if __name__ == "__main__":
+    s = input().strip()
     print(countGoodSubstrings(s))
