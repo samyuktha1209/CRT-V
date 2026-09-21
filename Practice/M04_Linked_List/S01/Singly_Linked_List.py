a9d372a2e976244cf2e70aad7c1bb36c3bd4f0d0
Samyuktha <samyukthachelli12@gmail.com>
2026-09-21 16:25:26 +0530

    singly

--- Practice/M04_Linked_List/S01/Singly_Linked_List.py
+++ Practice/M04_Linked_List/S01/Singly_Linked_List.py
@@ -6,7 +6,36 @@ DSA: The way of
 
 Application : library --> Sections --> Rows --> Books 
 
-what are the typess:
- 1. Prim
+
+what are the types:
+1.Primitives: int, float, char, string, boolean
+2.Non-Primitives:
+       a)Linear Data Structure: Array, Linked List, Stack, Queue
+       b)Non-Linear Data Structure: Tree, Graph
+
+       
+Linked List: The elements can be stored in the form od nodes ans each nodes have the co 
+Node: 2 parts
+   1. Data --> the actual value present
+   2.Reference --> this wil be points to next node
+
+Difference between List and Linked List:
+
+list                                                  |  Linked List
+
+1.Insertion and Deletion are difficult                | 1.Insertion and Deletion are very efficient
+2.Indexing                                            | 2.Non-Indexing
+3.Easy to traverse                                    | 3.Traverse each node
+
+3TYPES OF LINKED LIST:
+1.Singly Linked List
+2.Doubly Linked List
+3.Circular Linked List
+
+
+
+
+ 
+
 
 '''
