85e591a1d0e2b50cafce0525733013f8169d5584
Samyuktha <samyukthachelli12@gmail.com>
2026-09-21 16:22:55 +0530

    Assignment 2

---
+++ Practice/M04_Linked_List/S01/Singly_Linked_List.py
@@ -0,0 +1,12 @@
+'''
+DSA: The way of
+-->Storing
+-->Organizing
+--> Managing of data for efficient usage
+
+Application : library --> Sections --> Rows --> Books 
+
+what are the typess:
+ 1. Prim
+
+'''
